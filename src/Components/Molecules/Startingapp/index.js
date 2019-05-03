830e5d3314710f183fd49037ec15f01d4540f44c
river <riverhuang011@gmail.com>
2019-05-03 20:41:31 +0700

    weather

--- src/Components/Molecules/Startingapp/index.js
+++ src/Components/Molecules/Startingapp/index.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react'
 import {Button} from 'reactstrap'
+import logo from './../../../logo.svg'
+import './../../../App.css'
+
 export default class Startingapp extends Component {
     render() {
         return (
             <body className="bg-dark2">
                 <div >
                     <center left="50%" top="50%">
-                        <Button color="warning">start</Button>
+                    <img src={logo} className="App-logo"/>
+                    </center>
+                    <center left="50%" top="50%">
+                    <Button className="start" color="warning">start</Button>
                     </center>
                 </div>
             </body>
